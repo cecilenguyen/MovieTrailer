45fe8db9d01ebb3ef58fa6035982a326f32a0027
Uyen Nguyen <Uyen@Uyens-MBP.hitronhub.home>
2017-09-06 20:20:42 -0700

    added comments and formatting

--- media.py
+++ media.py
@@ -1,6 +1,11 @@
+''' Movie class creates a new movie object that
+    has a title, poster image, and trailer '''
+
+
 class Movie():
-	# constructor for a movie
-	def __init__(self, title, box_art, youtube_trailer):
-		self.title = title
-		self.poster_image_url = box_art
-		self.trailer_youtube_url = youtube_trailer
+    ''' Constructor for a movie object.
+    Requires a title, poster image, and trailer '''
+    def __init__(self, title, box_art, youtube_trailer):
+        self.title = title
+        self.poster_image_url = box_art
+        self.trailer_youtube_url = youtube_trailer
